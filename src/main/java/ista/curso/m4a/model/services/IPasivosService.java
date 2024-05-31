1c9e269f6098489eef24d5035bab0221c7d21a61
Miguel Zhunio est <miguel.zhunio.est@tecazuay.edu.ec>
2024-05-31 09:30:35 -0500

    Correción de la interface Pasivos

--- src/main/java/ista/curso/m4a/model/services/IPasivosService.java
+++ src/main/java/ista/curso/m4a/model/services/IPasivosService.java
@@ -6,7 +6,7 @@ import ista.curso.m4a.model.entity.Pasivos;
 
 public interface IPasivosService {
 	public List<Pasivos> findAll();
-	public Pasivos save(Pasivos socio);
+	public Pasivos save(Pasivos pasivo);
 	public Pasivos findById(Long id);
 	public void delete();
 }
