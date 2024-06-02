7470ff3e0e55166497efea57bd01a9904b268478
Miguel Zhunio est <miguel.zhunio.est@tecazuay.edu.ec>
2024-06-01 22:25:01 -0500

    Final de ingreso de datos

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -32,6 +32,7 @@ INSERT INTO `vehiculos` (`anio_fabricacion`, `valor`, `id`, `socio_id`, `marca`,
 INSERT INTO `vehiculos` (`anio_fabricacion`, `valor`, `id`, `socio_id`, `marca`, `numero_placa`, `tipo_vehiculo`) VALUES ('2009-06-20', '7200', NULL, '4', 'Nissan', 'AP-000216', 'Automovil');
 
 
+
 -- Datos Negocio
 INSERT INTO `negocios`(`antiguedad`, `hipotecado`, `numero_casa`, `numero_empleados`, `tiempo_local`, `socio_id`, `actividad`, `actividad_negocio`, `cuidad`, `direccion`, `institucion`, `locales`, `lugar_ventas`, `razon_social`, `referencia`, `ruc`, `subactividad`, `telefono`) VALUES (4 ,false ,345 ,10 ,6 ,1 ,'Vetna y compra de ropa','venta de ropa','Cuenca','Calle larga/Fabian Alarcon','N/A','Ninguno','Parque historico','Baratismo','Cerca del paque Calderón','11111','Compra de ropa usada','222-45-67');
 INSERT INTO `negocios`(`antiguedad`, `hipotecado`, `numero_casa`, `numero_empleados`, `tiempo_local`, `socio_id`, `actividad`, `actividad_negocio`, `cuidad`, `direccion`, `institucion`, `locales`, `lugar_ventas`, `razon_social`, `referencia`, `ruc`, `subactividad`, `telefono`) VALUES (1 ,true ,265 ,5 ,4 ,2 ,'Prestacion de servicios de internet','Servicios de internet','Machala','Juan Maldonado/Jep','BanEcuador','Ninguno','Parque de la Madre','InterPlus','Al frenete del Shopping','567896','Ninguna','123-567-86');
@@ -42,10 +43,25 @@ INSERT INTO `activos`(`bienes_inmuebles`, `cuentas_cobrar`, `depositos_financier
 INSERT INTO `activos`(`bienes_inmuebles`, `cuentas_cobrar`, `depositos_financieros`, `efectivo`, `herramientas_maquinaria`, `inventario_mercaderia`, `inversion_cultivos`, `inversion_ganado`, `muebles`, `total`, `vehiculos`, `socio_id`) VALUES (1000.00, 1000.00, 2000.00, 5000.00, 30000.00, 0.0, 1000.00, 0.0,800.00, 22000.00 ,8200.00,2);
 INSERT INTO `activos`(`bienes_inmuebles`, `cuentas_cobrar`, `depositos_financieros`, `efectivo`, `herramientas_maquinaria`, `inventario_mercaderia`, `inversion_cultivos`, `inversion_ganado`, `muebles`, `total`, `vehiculos`, `socio_id`) VALUES (1000.00, 1000.00, 2000.00, 1000.00, 20000.00, 0.0, 2000.00, 0.0,800.00, 19000.00 ,9200.00,3);
 
-
 -- Datos de Pasivos
 INSERT INTO `pasivos`(`deudas_almacenes`, `deudas_banco`, `deudas_cooperativas`, `deudas_otras`, `deudas_proovedores`, `deudas_tarjetas_credito`, `patromonio`, `total`, `total_pasivos`, `socio_id`) VALUES (1000.00, 1000.00, 0.0, 500.00, 500.00, 0.0,17000.00, 20000.00,3000.00,1);
 INSERT INTO `pasivos`(`deudas_almacenes`, `deudas_banco`, `deudas_cooperativas`, `deudas_otras`, `deudas_proovedores`, `deudas_tarjetas_credito`, `patromonio`, `total`, `total_pasivos`, `socio_id`) VALUES (1000.00, 1000.00, 0.0, 1000.00, 1000.00, 0.0, 18000.00, 22000.00,4000.00,2);
 INSERT INTO `pasivos`(`deudas_almacenes`, `deudas_banco`, `deudas_cooperativas`, `deudas_otras`, `deudas_proovedores`, `deudas_tarjetas_credito`, `patromonio`, `total`, `total_pasivos`, `socio_id`) VALUES (1000.00, 1000.00, 0.0, 5000.00, 0.0, 0.0,12000.00, 19000.00,7000.00,3);
+
+-- Datos Credito
+INSERT INTO `creditos`(`fecha_solicitud`, `plazo`, `valor_solicitado`, `socio_id`, `ciudad`, `estado`, `forma_pago`, `inversion`, `provincia`, `tipo_credito`)  VALUES ('2024-06-01', 1, 20000.00, 1, 'Cuenca', 'Habilitado', 'Mensual', 'dddd', 'Azuay', 'Microcredito');
+INSERT INTO `creditos`(`fecha_solicitud`, `plazo`, `valor_solicitado`, `socio_id`, `ciudad`, `estado`, `forma_pago`, `inversion`, `provincia`, `tipo_credito`)  VALUES ('2024-06-02', 1, 60000.00, 2, 'Cuenca', 'Habilitado', 'Mensual', 'dddd', 'Azuay', 'Supercredito');
+INSERT INTO `creditos`(`fecha_solicitud`, `plazo`, `valor_solicitado`, `socio_id`, `ciudad`, `estado`, `forma_pago`, `inversion`, `provincia`, `tipo_credito`)  VALUES ('2024-06-03', 1, 10000.00, 3, 'Cuenca', 'Habilitado', 'Mensual', 'dddd', 'Azuay', 'Microcredito');
+
+-- Datos de Ingresos
+INSERT INTO `ingresos`(`agricultura`, `alquiler_vehiculos`, `arriendos`, `comercio`, `ganaderia`, `intereses`, `otros_ingresos`, `pensiones`, `sueldo`, `sueldo_conyugue`, `total_ingresos`, `socio_id`) VALUES (0.0, 0.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0 ,4000.00, 0.0,4000.00, 1);
+INSERT INTO `ingresos`(`agricultura`, `alquiler_vehiculos`, `arriendos`, `comercio`, `ganaderia`, `intereses`, `otros_ingresos`, `pensiones`, `sueldo`, `sueldo_conyugue`, `total_ingresos`, `socio_id`) VALUES (100.0, 100.0, 0.0, 0.0, 0.0, 0.0,0.0, 0.0 ,2000.00, 0.0,2200.00, 2);
+INSERT INTO `ingresos`(`agricultura`, `alquiler_vehiculos`, `arriendos`, `comercio`, `ganaderia`, `intereses`, `otros_ingresos`, `pensiones`, `sueldo`, `sueldo_conyugue`, `total_ingresos`, `socio_id`) VALUES (0.0, 0.0, 0.0, 0.0, 0.0, 500.0,500.0, 0.0 ,1000.00, 0.0,2000.00, 3);
+
+-- Datos de gastos
+INSERT INTO `gastos`(`alimentacion`, `arriendo`, `deudas`, `educacion`, `otros_egresos`, `pensiones`, `salud`, `servicios_basico`, `total_gastos`, `transporte`, `vestimenta`, `socio_id`) VALUES (1000.00, 0.0, 0.0, 100.00 ,100.00 ,100.00,100.00 ,50.0,1600.00,50.00,100.00, 1);
+INSERT INTO `gastos`(`alimentacion`, `arriendo`, `deudas`, `educacion`, `otros_egresos`, `pensiones`, `salud`, `servicios_basico`, `total_gastos`, `transporte`, `vestimenta`, `socio_id`) VALUES (500.00, 0.0, 0.0, 0.0 ,100.00 ,0.0,0.0 ,50.0, 800.00,50.00,100.00, 2);
+INSERT INTO `gastos`(`alimentacion`, `arriendo`, `deudas`, `educacion`, `otros_egresos`, `pensiones`, `salud`, `servicios_basico`, `total_gastos`, `transporte`, `vestimenta`, `socio_id`) VALUES (500.00, 0.0, 0.0, 100.00 , 0.00 ,0.0,0.0 ,50.0,700.00,50.00,0.0, 3);
+
 
 
